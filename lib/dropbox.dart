6452dd468e9950c9d464d29a3da5f99c53b75ed8
mix1009 <mix1009@gmail.com>
2020-04-07 23:29:16 +0900

    add documentation, update yaml

--- lib/dropbox.dart
+++ lib/dropbox.dart
@@ -5,15 +5,26 @@ import 'package:flutter/services.dart';
 class Dropbox {
   static const MethodChannel _channel = const MethodChannel('dropbox');
 
+  /// Initialize dropbox library
+  ///
+  /// init() should be called only once.
   static Future<bool> init(String clientId, String key, String secret) async {
     return await _channel.invokeMethod(
         'init', {'clientId': clientId, 'key': key, 'secret': secret});
   }
 
+  /// Authorize using Dropbox app or web browser.
+  ///
+  /// Authorize using Dropbox app if it's installed. If not installed, it calls external web browser for authorization.
+  /// When user authorizes, no feedback is available. call getAccessToken() to check if authorized.
   static Future<void> authorize() async {
     await _channel.invokeMethod('authorize');
   }
 
+  /// Authorize with AccessToken
+  ///
+  /// use getAccessToken() to get Access Token after successful authorize().
+  /// authorizeWithAccessToken() will authorize without user interaction if access token is valid.
   static Future<void> authorizeWithAccessToken(String accessToken) async {
     await _channel
         .invokeMethod('authorizeWithAccessToken', {'accessToken': accessToken});
@@ -27,18 +38,28 @@ class Dropbox {
   //   return await _channel.invokeMethod('finishAuth', {'code': code});
   // }
 
+  /// get Access Token after authorization.
+  ///
+  /// returns null before authorization.
   static Future<String> getAccessToken() async {
     return await _channel.invokeMethod('getAccessToken');
   }
 
+  /// get account name
   static Future<String> getAccountName() async {
     return await _channel.invokeMethod('getAccountName');
   }
 
+  /// get folder/file list for path.
+  ///
+  /// returns List<dynamic>. Use path='' for accessing root folder. List items are not sorted.
   static Future listFolder(String path) async {
     return await _channel.invokeMethod('listFolder', {'path': path});
   }
 
+  /// get temporary link url for file
+  ///
+  /// returns url for accessing dropbox file.
   static Future<String> getTemporaryLink(String path) async {
     return await _channel.invokeMethod('getTemporaryLink', {'path': path});
   }
